30d28777a40bebc1eff85effd1c89dbd07c3a720
burimern <burimskijnikita6@gmail.com>
2024-07-16 03:15:14 +0300

    app.js

--- app.js
+++ app.js
@@ -7,21 +7,15 @@ tg.MainButton.color = '#2cab37';
 
 let items = [];
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
-let btn7 = document.getElementById("btn7");
+let buttons = document.querySelectorAll('.btn');
 
 function toggleItem(button, itemNumber) {
     if (items.includes(itemNumber)) {
         items = items.filter(item => item !== itemNumber);
-        button.classList.remove("selected"); // Необязательно: добавьте класс, чтобы указать, что кнопка выбрана.
+        button.classList.remove("selected");
     } else {
         items.push(itemNumber);
-        button.classList.add("selected"); // Необязательно: добавьте класс, чтобы указать, что кнопка выбрана.
+        button.classList.add("selected");
     }
     updateMainButton();
 }
@@ -35,32 +29,8 @@ function updateMainButton() {
     }
 }
 
-btn1.addEventListener("click", function() {
-    toggleItem(btn1, "1");
-});
-
-btn2.addEventListener("click", function() {
-    toggleItem(btn2, "2");
-});
-
-btn3.addEventListener("click", function() {
-    toggleItem(btn3, "3");
-});
-
-btn4.addEventListener("click", function() {
-    toggleItem(btn4, "4");
-});
-
-btn5.addEventListener("click", function() {
-    toggleItem(btn5, "5");
-});
-
-btn6.addEventListener("click", function() {
-    toggleItem(btn6, "6");
-});
-
-btn7.addEventListener("click", function() {
-    toggleItem(btn7, "7");
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => toggleItem(button, (index + 1).toString()));
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
@@ -74,6 +44,7 @@ let p = document.createElement("p");
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
+
 
 
 
